9950ef48b47bd31c26f252b1a12e4b4c7698e1ae
Tandy Sabana <tansabana12@gmail.com>
2024-07-07 15:04:22 +0700

    Initial Commit
    Initial Commit

---
+++ MemorizeApp/MemorizeApp/MemorizeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeAppApp.swift
+//  MemorizeApp
+//
+//  Created by tandyys on 07/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
