e8d499d0a9b8d75f63f06b5fae1062df8cfdd66c
Tandy Sabana <tansabana12@gmail.com>
2024-07-07 16:22:03 +0700

    Array-ing the content & add-remove card system
    Array-ing the content & add-remove card system

--- MemorizeApp/MemorizeApp/ContentView.swift
+++ MemorizeApp/MemorizeApp/ContentView.swift
@@ -8,31 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let emojis:[String] = ["🍎", "💀", "👾", "🪼", "🐊", "🧟", "🧸", "🦠"]
+    @State var cardCount: Int = 0
+    
     var body: some View {
         VStack {
-            CardView(isFaceUp: true)
-            CardView()
+            HStack {
+                ForEach(0..<cardCount, id: \.self) { index in
+                    CardView(content: emojis[index])
+                }
+            }
+            .foregroundColor(.red)
+            .padding()
+            HStack(alignment: .center, content: {
+                Button(action: {
+                    if cardCount < emojis.count {
+                        cardCount+=1
+                    }
+                }, label: {
+                    Image(systemName: "plus.rectangle")
+                })
+                Spacer()
+                Button(action: {
+                    if cardCount > 1 {
+                        cardCount-=1
+                    }
+                }, label: {
+                    Image(systemName: "minus.rectangle")
+                })
+            })
+            .foregroundColor(.red)
+            .imageScale(.large)
+            .font(.largeTitle)
         }
-        .foregroundColor(.orange)
-        .font(.largeTitle)
         .padding()
     }
+    
 }
 
 struct CardView: View {
-    var isFaceUp:Bool = false
+    
+    var content:String
+    @State var isFaceUp:Bool = false
     
     var body: some View {
         ZStack {
+            var baseCard: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12).foregroundStyle(Color.white)
-                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 3)
-                Text("🍎")
+                baseCard.fill(.white)
+                baseCard.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius: 12)
+                baseCard.fill()
             }
         }
+        .onTapGesture {
+            isFaceUp.toggle()
+            print(isFaceUp)
+        }
     }
+    
 }
 
 #Preview {
